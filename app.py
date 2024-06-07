68a0f86a48de99c678c183d2c4cc7e7a53a80934
parthpatel2374 <dcpatel577@gmail.com>
2024-06-07 18:55:27 +0530

    Deleted unused files.

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
-from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from db import db
 from models import User
